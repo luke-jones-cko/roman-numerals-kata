9c01b669f5bd2d3dc7eb35e8c3592e2a2aaa92b6
aiden-ramchande-cko <aiden.ramchande@checkout.com>
2022-01-26 15:48:13 +0000

    add logic to cover cases up to 11

--- RomanNumerals/RomanNumerals/IntegerExtensions.cs
+++ RomanNumerals/RomanNumerals/IntegerExtensions.cs
@@ -1,9 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
 namespace RomanNumerals;
 
 public static class IntegerExtensions
 {
+
+    private static readonly Dictionary<int, string> numeralSymbols;
+    private static bool shouldBeReversed = false;
+
+    static IntegerExtensions()
+    {
+        numeralSymbols = new Dictionary<int, string>()
+        {
+            {1, "I"},
+            {5, "V"},
+            {10, "X"}
+        };
+    }
+
+
     public static string ToRomanNumerals(this int number)
     {
-        return string.Empty;
+        // 1, 5, 10
+        if (numeralSymbols.ContainsKey(number))
+        {
+            return numeralSymbols[number];
+        }
+        
+        var sb = new StringBuilder();
+
+        for (int i = 0; i <= number; i++)
+        {
+            if (number != 0)
+            {
+                var hv = GetHighestValue(number);
+                
+                if (hv == 0) break;
+                
+                sb.Append(numeralSymbols[hv]);
+                number = number - hv;
+                if (number is < 0 or 1)
+                {
+                    sb.Append(numeralSymbols[1]);
+                    i = 0;
+                }
+            }
+        }
+        
+        return shouldBeReversed ? ReverseString(sb.ToString()) : sb.ToString();
+    }
+
+    private static string ReverseString(string s)
+    {
+        char[] array = s.ToCharArray();
+        Array.Reverse(array);
+        return new string(array);
+    }
+
+    private static int GetHighestValue(int number)
+    {
+        
+        foreach (var symbol in numeralSymbols.Keys.Reverse())
+        {
+            if (number + 1 == symbol)
+            {
+                shouldBeReversed = true;
+                return symbol;
+            }
+
+            if (number > symbol)
+            {
+                return symbol;
+            }
+        }
+
+        return 0;
     }
 }
--- RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
@@ -7,6 +7,15 @@ public class RomanNumeralsTests
 {
     [Theory]
     [InlineData(1, "I")]
+    [InlineData(2, "II")]
+    [InlineData(3, "III")]
+    [InlineData(4, "IV")]
+    [InlineData(5, "V")]
+    [InlineData(6, "VI")]
+    [InlineData(7, "VII")]
+    [InlineData(8, "VIII")]
+    [InlineData(10, "X")]
+    [InlineData(11, "XI")]
     public void ToRomanNumerals(int number, string expected)
     {
         number.ToRomanNumerals().Should().Be(expected);
