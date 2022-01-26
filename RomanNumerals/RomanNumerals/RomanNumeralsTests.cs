b0047eefb3cd54b1a2b45cdb7200fa90b4bb369b
Kevin Li <kevin.li@checkout.com>
2022-01-26 14:21:09 +0000

    -

--- RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
@@ -7,6 +7,7 @@ public class RomanNumeralsTests
 {
     [Theory]
     [InlineData(1, "I")]
+    [InlineData(2, "II")]
     public void ToRomanNumerals(int number, string expected)
     {
         number.ToRomanNumerals().Should().Be(expected);
