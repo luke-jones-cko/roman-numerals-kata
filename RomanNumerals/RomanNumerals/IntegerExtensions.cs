eb54ce031c3db0c6c931194bbc5f1548d9582a0a
Kevin Li <kevin.li@checkout.com>
2022-01-26 14:20:18 +0000

    -

--- RomanNumerals/RomanNumerals/IntegerExtensions.cs
+++ RomanNumerals/RomanNumerals/IntegerExtensions.cs
@@ -1,9 +1,18 @@
+using System;
+
 namespace RomanNumerals;
 
 public static class IntegerExtensions
 {
     public static string ToRomanNumerals(this int number)
     {
+        var romanNumber = "I";
+
+        if (number == 1)
+        {
+            return romanNumber;
+        }
+
         return string.Empty;
     }
 }
