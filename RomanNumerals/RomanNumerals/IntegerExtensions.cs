6db8a1c0126b65f993b299c72e66f8d494e69e44
Trev Davies <trev.davies@checkout.com>
2022-01-26 14:28:35 +0000

    Try 5

--- RomanNumerals/RomanNumerals/IntegerExtensions.cs
+++ RomanNumerals/RomanNumerals/IntegerExtensions.cs
@@ -6,13 +6,6 @@ public static class IntegerExtensions
 {
     public static string ToRomanNumerals(this int number)
     {
-        var romanNumber = "I";
-
-        if (number == 1)
-        {
-            return romanNumber;
-        }
-
-        return string.Empty;
+        return number == 1 ? "I" : "II";
     }
 }
--- RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
@@ -1,14 +1,15 @@
-namespace RomanNumerals;
-
 using FluentAssertions;
 using Xunit;
+
+namespace RomanNumerals;
 
 public class RomanNumeralsTests
 {
     [Theory]
     [InlineData(1, "I")]
     [InlineData(2, "II")]
-    public void ToRomanNumerals(int number, string expected)
+    [InlineData(5, "V")]
+    public void ToRomanNumeralsTest(int number, string expected)
     {
         number.ToRomanNumerals().Should().Be(expected);
     }
